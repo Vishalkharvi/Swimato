c5e0d423d55f0064111a563aaac4c174b82c8e20
Vishalkharvi <49681770+Vishalkharvi@users.noreply.github.com>
2021-07-22 23:35:18 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,29 +6,29 @@ const Card = (props) => {
 
     <div className='col-12 mx-auto col-md-4 col-lg-3  itemBorder px-0 py-0'>
         <div className='row rowToPad'>
-            <div className='col-12 col-md-12 col-lg-5 mt-1 my-lg-5 imgCss  pb-0 pt-0 '> 
+            <div className='col-12 col-md-12 col-lg-5 mt-lg-2 imgCss  pb-0 pt-0 '> 
                 <img className='bg-warning img-fluid pt-0' src={props.image} alt={props.name} />
             </div>
 
 
-            <div className='col-12 col-md-12 col-lg-7' style={{color:'yellow',}}>
-                <div className='pt-4 pb-3'>
-                    <p className='title'> {props.name}</p>
+            <div className='col-12 col-md-12 col-lg-7 px-lg-1 px-5' style={{color:'yellow',}}>
+                <div className='pt-2 pb-2'>
+                    <p className='title text-center'> {props.name}</p>
 
                     <p>{props.description}</p>
                 </div>
+            </div>
+        </div>
 
-                <div className='d-flex f-row justify-content-between'>
-
+            <div className='row '>
+                <div className='col-12 col-md-6 col-lg-12 mx-auto d-flex flex-row justify-content-between px-5 py-2'>
                     <p className='priceTag'> {props.price}</p>
                     <button className='buttonClass bg-warning mb-1'>Buy</button>
                 </div>
-
             </div>
 
 
                                 
-        </div>
     </div>
         </>
     );
